49e5963c74cc3fd9835b2cd2a87f25261e83e47d
Rhiswana <rhiswanabegam@gmail.com>
2026-02-09 04:25:35 +0000

    landing page

---
+++ cooksmart-app/src/app/components/landing/landing.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router,RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+  // Navigation methods
+  goToLogin(): void {
+    console.log('Navigate to login');
+    
+  }
+
+  goToSignup(): void {
+    console.log('Navigate to signup');
+    
+  }
+}
